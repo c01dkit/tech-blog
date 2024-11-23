225b1f8d698bd405661275d47b36033a314d28df
c01dkit <contact@c01dkit.com>
2024-11-23 16:31:30 +0800

    Deployed 820332b with MkDocs version: 1.6.1

---
+++ js/oml2d.js
@@ -0,0 +1,14 @@
+OML2D.loadOml2d({
+  parentElement: document.body,
+  models: [
+    {
+      path: "/oh-my-mkdocs/dev/ATRI/atri_8.model3.json",
+      position: [-100, 0],
+      scale: 0.2,
+      stageStyle: {
+        bottom: 0,
+        left:0,
+      },
+    },
+  ],
+});
